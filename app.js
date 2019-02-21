f1bab57761b0edca0037205d416611437c524cc3
Rian Wardana <riaanwardana@gmail.com>
2019-02-21 20:14:14 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+(function() {
+
+    client = new Paho.MQTT.Client("test.mosquitto.org", Number(8080), Math.random().toString(16).replace(/[^a-z]+/g, '').substr(0, 5));
+    client.onConnectionLost = onConnectionLost;
+    client.onMessageArrived = onMessageArrived;
+    client.connect({onSuccess:onConnect});
+
+    function onConnect() {
+        console.log("Komunikasi terhubung.");
+        client.subscribe("proyekdo/001/oxy");
+        client.subscribe("proyekdo/001/temp");
+    }
+
+    function onConnectionLost() {
+        alert("Koneksi terputus.");
+    }
+
+    function onMessageArrived(message) {
+        console.log(message);
+
+        if (message.destinationName == "proyekdo/001/oxy") oxy.innerHTML = message.payloadString;
+        else if (message.destinationName == "proyekdo/001/temp") temp.innerHTML = message.payloadString;
+    }
+}())
